d0d71ff7b58750da9d19e0075cfd0d20149204a3
Andrey Zakharevich <andreyzakharevich@gmail.com>
2022-04-03 11:51:09 +0300

    Reformat files

--- solvers/versionDemo.ml
+++ solvers/versionDemo.ml
@@ -3,44 +3,47 @@ open Versions
 open Program
 open Utils
 
-let _ : unit =
-  let t = new_version_table() in
+let (_ : unit) =
+  let t = new_version_table () in
   let p = "(#(lambda (lambda (* $2 (+ (lambda $2) $0)))) $0 2)" |> parse_program |> get_some in
-  p |> incorporate t |> inline t |> extract t |> List.iter ~f:(fun p' ->
-      Printf.printf "%s\n"
-        (string_of_program p'));
-  assert (false)
-;;
+  p |> incorporate t |> inline t |> extract t
+  |> List.iter ~f:(fun p' -> Printf.printf "%s\n" (string_of_program p'));
+  assert false
 
+let (_ : unit) =
+  List.range 0 6
+  |> List.iter ~f:(fun sz ->
+         let p0 =
+           List.range 0 sz
+           |> List.fold_right ~init:"(+ 1 1)" ~f:(fun _ -> Printf.sprintf "(+ 1 %s)")
+           |> parse_program |> get_some
+         in
 
-let _ : unit =
-  List.range 0 6 |> List.iter ~f:(fun sz ->
-      let p0 = List.range 0 sz |>
-               List.fold_right ~init:"(+ 1 1)" ~f:(fun _ -> Printf.sprintf "(+ 1 %s)") |>
-               parse_program |> get_some
-      in
+         List.range 1 4
+         |> List.iter ~f:(fun a ->
+                let v = new_version_table () in
+                let j = incorporate v p0 in
+                let[@warning "-8"] r =
+                  List.range 0 a
+                  |> List.fold_right ~init:[ j ] ~f:(fun _ (a :: b) ->
+                         recursive_inversion v a :: a :: b)
+                  |> union v
+                in
 
-      List.range 1 4 |> List.iter ~f:(fun a ->
-          let v = new_version_table() in
-          let j = incorporate v p0 in
-          let [@warning "-8"] r = List.range 0 a |>
-                  List.fold_right ~init:[j] ~f:(fun _ (a :: b) -> recursive_inversion v a :: a :: b) |>
-                  union v
-          in
+                let version_size : int = reachable_versions v [ r ] |> List.length in
 
-          let version_size : int = reachable_versions v [r] |> List.length in
+                let ht = new_version_table () in
+                let distinct_programs =
+                  extract v r
+                  |> List.map ~f:(incorporate ht)
+                  |> List.dedup_and_sort ~compare:( - )
+                  |> List.length
+                in
+                let program_memory = version_table_size ht in
 
-          let ht = new_version_table() in
-          let distinct_programs = extract v r |> List.map ~f:(incorporate ht) |> List.dedup_and_sort ~compare:(-) |> List.length in
-          let program_memory = version_table_size ht in
-
-          Printf.printf "version_size[%d,%d] = %d\n"
-            sz a version_size;
-          Printf.printf "distinct_programs[%d,%d] = %d\n"
-            sz a distinct_programs;
-          Printf.printf "program_memory[%d,%d] = %d\n"
-            sz a program_memory;
-          (* Printf.printf "approximate size = %f\n" *)
-          (*   (unique_space v r |> log_version_size v |> exp); *)
-          flush_everything()
-        ))
+                Printf.printf "version_size[%d,%d] = %d\n" sz a version_size;
+                Printf.printf "distinct_programs[%d,%d] = %d\n" sz a distinct_programs;
+                Printf.printf "program_memory[%d,%d] = %d\n" sz a program_memory;
+                (* Printf.printf "approximate size = %f\n" *)
+                (*   (unique_space v r |> log_version_size v |> exp); *)
+                flush_everything ()))
